7640ff0d77dab0c2c8a9ae30596ce792449614b0
Zisouu <47763201+Zisouu@users.noreply.github.com>
2022-03-23 21:26:08 +0900

    feat : viewModel DI

--- app/src/main/java/com/study/tastingnote/di/ViewModelModule.kt
+++ app/src/main/java/com/study/tastingnote/di/ViewModelModule.kt
@@ -1,11 +1,14 @@
 package com.study.tastingnote.di
 
 import com.study.tastingnote.viewModel.LiquorFragmentViewModel
+import com.study.tastingnote.viewModel.LiquorWriteListFragmentViewModel
+import com.study.tastingnote.viewModel.WriteTrackViewModel
 import org.koin.core.context.GlobalContext.get
 import org.koin.dsl.module
 import org.koin.androidx.viewmodel.dsl.viewModel
 val viewModelModule = module {
 
     viewModel { LiquorFragmentViewModel(get(), get()) }
-
+    viewModel { WriteTrackViewModel(get()) }
+    viewModel { LiquorWriteListFragmentViewModel(get())}
 }
